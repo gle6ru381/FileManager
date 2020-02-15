e0fff29d29863cd306752dd5930996269095c04a
Глеб Трогаев <trogaev@bk.ru>
2020-02-15 21:10:40 +0700

    add definition constructor MainWindow and slots

--- Bin/mainwindow.cpp
+++ Bin/mainwindow.cpp
@@ -1,11 +1,72 @@
 #include "mainwindow.h"
+#include <QCoreApplication>
+#include <QFileSystemModel>
+#include <QString>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+#define MAINPATH "D:/reposQt/FileManager"
+
+MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent)
 {
+    topBar = new QToolBar(this);
+    topBar->setAllowedAreas(Qt::TopToolBarArea);
+    topBar->setMovable(false);
+    topBar->setFloatable(false);
+    back = new QPushButton(topBar);
+    back->setEnabled(false);
+    back->setIcon(QIcon(QString("%1/pics/go-previous.png").arg(MAINPATH)));
+    back->setIconSize(QSize(40, 40));
+    connect(back, SIGNAL(released()), this, SLOT(pressBack()));
+    topBar->addWidget(back);
+    addToolBar(Qt::TopToolBarArea, topBar);
+
+    QFileSystemModel* model = new QFileSystemModel(this);
+    model->setRootPath("");
+    model->setReadOnly(false);
+    past.push_back(model->index(""));
+
+    QTreeView* tree = new QTreeView(this);
+    tree->setModel(model);
+    tree->setRootIndex(past.first());
+    tree->hideColumn(1);
+    tree->hideColumn(2);
+    tree->hideColumn(3);
+
+    leftBar = new QToolBar(this);
+    leftBar->setOrientation(Qt::Vertical);
+    leftBar->setAllowedAreas(Qt::LeftToolBarArea);
+    leftBar->setMaximumSize(150, 500);
+    leftBar->addWidget(tree);
+    leftBar->setMovable(false);
+    leftBar->setFloatable(false);
+    addToolBar(Qt::LeftToolBarArea, leftBar);
+
+    mainTree = new QTreeView(this);
+    mainTree->setModel(model);
+    mainTree->setRootIndex(past.first());
+    mainTree->setTreePosition(5);
+    connect(mainTree,
+            SIGNAL(doubleClicked(const QModelIndex&)),
+            this,
+            SLOT(changedList(const QModelIndex&)));
+
+    setCentralWidget(mainTree);
+    setGeometry(0, 30, 800, 800);
 }
 
-MainWindow::~MainWindow()
+void MainWindow::changedList(const QModelIndex& index)
 {
+    back->setEnabled(true);
+    past.push_back(index);
+    mainTree->setRootIndex(index);
 }
 
+void MainWindow::pressBack()
+{
+    if (past.begin() != past.end() - 1) {
+        past.removeLast();
+        mainTree->setRootIndex(past.last());
+        if (past.first() == past.last())
+            back->setEnabled(false);
+    } else
+        back->setEnabled(false);
+}
--- Bin/mainwindow.h
+++ Bin/mainwindow.h
@@ -1,14 +1,27 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QListView>
 #include <QMainWindow>
+#include <QPushButton>
+#include <QToolBar>
+#include <QTreeView>
+#include <QVector>
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    MainWindow(QWidget* parent = nullptr);
+
+private:
+    QToolBar* topBar;
+    QToolBar* leftBar;
+    QVector<QModelIndex> past;
+    QPushButton* back;
+    QTreeView* mainTree;
+private slots:
+    void changedList(const QModelIndex& index);
+    void pressBack();
 };
 #endif // MAINWINDOW_H
